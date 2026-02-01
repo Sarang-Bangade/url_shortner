f231638a6cd083cd3f2e798aa70a29644ddf327a
Sarang-Bangade <bangadesarang@gmail.com>
2026-02-01 23:46:38 +0530

    first initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+import random
+import string
+from models import (
+                    init_db,
+                    insert_url,
+                    get_all_urls,
+                    get_url,
+                    increment_visit_count,
+                    delete_url_by_code
+                 )
+app = Flask(__name__)
+
+init_db()
+
+def generate_short_code():
+    
+
+@app.route("/")
+def hello_world():
+    return 'Hello this is Sarang from Flask'
+
+@app.route("/about")
+def about_us(): 
+    return 'Hello this is About us page'
+
+if __name__ == '__main__':
+    app.run(debug=True)
