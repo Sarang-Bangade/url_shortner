62178547f7addcd30bf61fc2161f007e685eda43
Sarang-Bangade <bangadesarang@gmail.com>
2026-02-02 12:27:26 +0530

    crud operations, handling get and Post requests, handling jinga templates and frontend

--- models.py
+++ models.py
@@ -5,8 +5,8 @@ def init_db():
     with sqlite3.connect(DB_NAME) as conn:
         conn.execute('''
 
-        CREATE TABLE IF NOT EXISTES urlS(
-                    id INTEGER PRIMARTY KETY AUTOINCREMENT,
+        CREATE TABLE IF NOT EXISTS urls(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
                     original_url TEXT NOT NULL,
                     short_code TEXT UNIQUE NOT NULL,
                     visit_count INTEGER DEFAULT 0
@@ -31,7 +31,7 @@ def increment_visit_count(short_code):
         UPDATE urls
         SET visit_count = visit_count + 1
         WHERE short_code = ?
-        ''',(short_code))  
+        ''',(short_code,))  
 
 def get_all_urls():
     with sqlite3.connect(DB_NAME) as conn:
@@ -41,4 +41,4 @@ def get_all_urls():
 
 def  delete_url_by_code(short_code):
     with sqlite3.connect(DB_NAME) as conn:
-        conn.execute('DELETE from urls WHERE short_code = ?',(short_code))
+        conn.execute('DELETE from urls WHERE short_code = ?',(short_code,))
